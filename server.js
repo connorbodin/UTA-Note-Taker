f536408b51edf896cee91e206fd35bb0fe33a9ec
connorbodin <connor.e.bodin@gmail.com>
2023-02-02 14:50:41 -0600

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const PORT = process.env.port || 3003;
+const PORT = process.env.PORT || 3003;
 const { v4: uuidv4 } = require('uuid');
 // call uuid uuidv4();
 const util = require('util');
